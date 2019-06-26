f8ea31eb725d4aa463cdbf04bcd141a301525447
FaustFox <faustffox@gmail.com>
2019-06-26 17:43:12 +0300

    edit test script

--- test_items.py
+++ test_items.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 import pytest
-import time
 from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_see_add_to_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
     browser.find_element_by_css_selector(".btn-add-to-basket")
